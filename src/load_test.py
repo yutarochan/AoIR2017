3544fa37f217a7e2a129d6415b0dc398f96f7329
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-22 20:51:48 -0500

    fix

--- src/load_test.py
+++ src/load_test.py
@@ -15,7 +15,7 @@ sc = SparkContext()
 raw_data = sc.textFile("hdfs:///user/yjo5006/AOIR2017/data/clinton-20161019.json")
 json_data = raw_data.map(lambda x: json.loads(x))
 
-print 'TOTAL TWEETS LOADED: ' + str(text_file.count())
+print 'TOTAL TWEETS LOADED: ' + str(json_data.count())
 
 # Preprocessing Phase
 raw_tweets = json_data.map(lambda x: filter(None, [re.sub(r'[^A-Za-z0-9]+', '', x), tk.simple_tokenize(x['text'])]))
