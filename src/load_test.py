d9e87c28bf6b42c5adb7df8bd7f10d5e0301bca7
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2017-02-23 13:42:45 -0500

    Append stopword filter

--- src/load_test.py
+++ src/load_test.py
@@ -18,10 +18,14 @@ json_data = raw_data.map(lambda x: json.loads(x))
 print 'TOTAL TWEETS LOADED: ' + str(json_data.count())
 
 # Preprocessing Phase
-
-tokens = json_data.map(lambda x: tk.simple_tokenize(x['text']))                                 # Tokenize
+tokens = json_data.map(lambda x: tk.simple_tokenize(x['text'].lower()))                         # Tokenize & Normalize
 tokens = tokens.map(lambda tok: filter(None, [re.sub(r'[^A-Za-z0-9]+', '', x) for x in tok]))   # Remove Empty Strings
 tokens = tokens.map(lambda tok: filter(lambda x: x.startswith('http') == False, tok))           # Remove HTTP URLS
-tokens = tokens.map(lambda tok: filter(lambda x: x != 'RT', tok))                               # Filter RT Token
+tokens = tokens.map(lambda tok: filter(lambda x: x != 'rt', tok))                               # Filter RT Token
+
+# Remove Stopwords
+stopwords = open('res/stopwords.txt', 'rb')
+stop = sc.broadcast(stopwords.read().split('\n')[:-1])
+tokens = tokens.map(lambda tok: [t for t in tok if t not in stop.value])
 
 print tokens.take(1)
